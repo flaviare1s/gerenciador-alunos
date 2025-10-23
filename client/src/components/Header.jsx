b88806fe0d3f9e30f15d7b25333e612b98e1a3d5
flaviare1s <flaviarster@gmail.com>
2025-10-23 13:57:12 -0300

    style(front): header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,14 @@
+import logo from "../assets/img/Logo.png"
+
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header className="bg-primary">
+      <div className="py-[18px] px-[42px] flex justify-between items-center">
+        <div className="flex items-center gap-[25px]">
+          <img src={logo} alt="Logo" />
+          <h1 className="text-white font-extrabold tracking-[1%] leading-[30px]">Gerenciador de alunos</h1>
+        </div>
+      </div>
+    </header>
   )
 }
