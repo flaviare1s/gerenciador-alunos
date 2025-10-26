0f3c789616359e2f1ad8ede5e55dc5ffc3007e1f
flaviare1s <flaviarster@gmail.com>
2025-10-26 09:12:01 -0300

    feat(header): adjust header to show/hide elements based on route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,36 @@
-import logo from "../assets/img/Logo.png"
+import { Link, useLocation } from "react-router-dom";
+import logo from "../assets/img/Logo.png";
+import trash from "../assets/img/Trash.png";
+import { IoIosArrowBack } from "react-icons/io";
 
 export const Header = () => {
+  const location = useLocation();
+  const isRoot = location.pathname === "/";
+
   return (
     <header className="bg-primary">
       <div className="py-[18px] px-[42px] flex justify-between items-center">
         <div className="flex items-center gap-[25px]">
-          <img src={logo} alt="Logo" />
-          <h1 className="text-white font-extrabold tracking-[1%] leading-[30px]">Gerenciador de alunos</h1>
+          {!isRoot && (
+            <Link to="/">
+              <IoIosArrowBack className="text-white text-2xl" />
+            </Link>
+          )}
+          {isRoot && (
+            <Link to="/">
+              <img src={logo} alt="Logo" />
+            </Link>
+          )}
+          <h1 className="text-white font-extrabold tracking-[1%] leading-[30px]">
+            Gerenciador de alunos
+          </h1>
         </div>
+        {!isRoot && (
+          <button className="cursor-pointer w-[17.5px] h-[17.5px]">
+            <img className="w-full" src={trash} alt="Trash" />
+          </button>
+        )}
       </div>
     </header>
-  )
-}
+  );
+};
