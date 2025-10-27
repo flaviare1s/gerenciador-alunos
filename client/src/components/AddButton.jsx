83cae2639b9a95f6e904c1cff3631af09450b68f
flaviare1s <flaviarster@gmail.com>
2025-10-27 16:13:05 -0300

    refactor: rename routes in AppRoutes

--- client/src/components/AddButton.jsx
+++ client/src/components/AddButton.jsx
@@ -3,7 +3,7 @@ import addIcon from "../assets/img/Add.png"
 export const AddButton = () => {
   return (
     <Link
-      to="/cadastro-student"
+      to="/cadastro-aluno"
       className="flex items-center justify-center gap-2 py-2 px-6 text-neutral-black text-sm border border-gray-medium rounded-md cursor-pointer hover:bg-gray-light hover:text-primary w-full sm:w-auto"
     >
       <span className="w-6 h-6">
