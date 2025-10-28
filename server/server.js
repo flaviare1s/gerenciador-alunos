5501a8d337d75b444cc52f01b8b9d093604712e4
flaviare1s <flaviarster@gmail.com>
2025-10-28 07:49:14 -0300

    docs: add Swagger documentation for course routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import routes from "./src/routes/index.js";
+import { swaggerUi, swaggerSpec } from "./swagger.js";
 
 dotenv.config();
 
@@ -14,7 +15,10 @@ app.use(express.json());
 
 app.use("/api", routes);
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
+  console.log("Documentação disponível em http://localhost:3000/api-docs");
 });
